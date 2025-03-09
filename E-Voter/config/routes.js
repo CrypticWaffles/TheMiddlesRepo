5bc27bddc1f7e1cf58bcfefa76a1d3e6972380c0
Miles <milesbgriffith@gmail.com>
2025-03-08 17:26:02 -0800

    re-add vote routes

--- E-Voter/config/routes.js
+++ E-Voter/config/routes.js
@@ -19,4 +19,6 @@ module.exports.routes = {
   // Route for playing a specific video
   'GET /video/play/:id': 'VideoController.play',
   '/vote': { view: 'pages/vote' },
+  'POST /vote-entryYes': 'VoteController.voteEntryYes',
+  'POST /vote-entryNo': 'VoteController.voteEntryNo',
 };
